72038be01ea171291680b4111cd1d8e6200af976
Chris Motes <cmotes@Acumeras-MacBook-Air.local>
2016-11-07 10:57:41 -0600

    user data will stop the node and we need a lambda trigger to create the ami.

--- user_data.sh
+++ user_data.sh
@@ -3,3 +3,8 @@
 ## insert node build script
 instanceid=`curl -s curl http://169.254.169.254/latest/meta-data/instance-id` 
 NAME=`aws --region us-east-1 ec2 describe-tags --filters "Name=resource-id,Values=$instanceid" "Name=key,Values=Name" --query 'Tags[0].Value' --output text` 
+sudo yum -y install https://yum.puppetlabs.com/puppetlabs-release-pc1-el-6.noarch.rpm
+sudo yum clean all
+sudo yum -y install puppet-agent
+yum -q -y update
+init 0
