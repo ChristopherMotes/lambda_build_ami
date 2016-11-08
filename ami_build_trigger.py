86c4f67b226b10ec6b12adf628922541133da2f7
Christopher Motes <motescj@aim.com>
2016-11-08 17:23:45 -0600

    added lambda pieces

--- ami_build_trigger.py
+++ ami_build_trigger.py
@@ -1,35 +1,35 @@
 import boto3
 import time
-
-DATEYMD = time.strftime("%Y-%m-%d")
-#DATEYMD = '2016-08-08'
-print DATEYMD
-session = boto3.Session(profile_name='shaeking1')
-ec2 = session.client('ec2')
-imagesDict = ec2.describe_images(
-	Owners=[
-		'amazon',
-	],
-	Filters=[
-		{ 
-			'Name' : 'name',
-			'Values' : [ '*amazon-ecs-optimized' ]
-		},
-		{
-			'Name' : 'state',
-			'Values' : [
-				'pending',
-				'available'
-			]
-		},
-		{
-			'Name' : 'image-type',
-			'Values' : [ 'machine' ]
-		}
-	]
-)
+def lambda_handler(event, context):
+    DATEYMD = time.strftime("%Y-%m-%d")
+    #DATEYMD = '2016-08-08'
+    print DATEYMD
+    ec2 = boto3.client('ec2')
+    imagesDict = ec2.describe_images(
+    	Owners=[
+    		'amazon',
+    	],
+    	Filters=[
+    		{ 
+    			'Name' : 'name',
+    			'Values' : [ '*amazon-ecs-optimized' ]
+    		},
+    		{
+    			'Name' : 'state',
+    			'Values' : [
+	    			'pending',
+		    		'available'
+			    ]
+    		},
+    		{
+	    		'Name' : 'image-type',
+		    	'Values' : [ 'machine' ]
+		    }
+	    ]
+    )
  
-for images in imagesDict['Images'] :
-	if DATEYMD in images['CreationDate']:
-		print images['ImageId']
-
+    for images in imagesDict['Images'] :
+    	if DATEYMD in images['CreationDate']:
+	    	print images['ImageId']
+    
+    return 'Function Complete'
