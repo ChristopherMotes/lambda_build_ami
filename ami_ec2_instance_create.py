1e68a4c73f07ba5a538fadcf4f10411cee009342
Chris Motes <cmotes@Acumeras-MacBook-Air.local>
2016-11-08 09:57:28 -0600

    rule created, didnt get instanceID

--- ami_ec2_instance_create.py
+++ ami_ec2_instance_create.py
@@ -1,11 +1,11 @@
 import boto3
 
+session = boto3.Session(profile_name='shaeking1')
 TAG_Name='test-tag'
 TAG_VPC='default'
 User_Data='IyEvYmluL2Jhc2gKIyMgQmFzZTY0IGVuY29kZSBtZSwgdGhlbiBhcHBseSBiYXNlNjQgb3V0cHV0IHRvIHRoZSB1c2VyIGRhdGEgc2VjdGlvbgojIyBpbnNlcnQgbm9kZSBidWlsZCBzY3JpcHQKaW5zdGFuY2VpZD1gY3VybCAtcyBjdXJsIGh0dHA6Ly8xNjkuMjU0LjE2OS4yNTQvbGF0ZXN0L21ldGEtZGF0YS9pbnN0YW5jZS1pZGAgCk5BTUU9YGF3cyAtLXJlZ2lvbiB1cy1lYXN0LTEgZWMyIGRlc2NyaWJlLXRhZ3MgLS1maWx0ZXJzICJOYW1lPXJlc291cmNlLWlkLFZhbHVlcz0kaW5zdGFuY2VpZCIgIk5hbWU9a2V5LFZhbHVlcz1OYW1lIiAtLXF1ZXJ5ICdUYWdzWzBdLlZhbHVlJyAtLW91dHB1dCB0ZXh0YCAKc3VkbyB5dW0gLXkgaW5zdGFsbCBodHRwczovL3l1bS5wdXBwZXRsYWJzLmNvbS9wdXBwZXRsYWJzLXJlbGVhc2UtcGMxLWVsLTYubm9hcmNoLnJwbQpzdWRvIHl1bSBjbGVhbiBhbGwKc3VkbyB5dW0gLXkgaW5zdGFsbCBwdXBwZXQtYWdlbnQKeXVtIC1xIC15IHVwZGF0ZQppbml0IDAK'
 
 
-session = boto3.Session(profile_name='shaeking1')
 ec2 = session.client('ec2')
 instanceDict = ec2.run_instances(
 	ImageId='ami-b73b63a0',
@@ -27,9 +27,13 @@ instanceDict = ec2.run_instances(
 	
 for instance in instanceDict['Instances']:
 	instanceID =  instance['InstanceId']
-events=Session.boto3.client('events')
+events=session.client('events')
 response = events.put_rule(
 	Name='ami-auto-build',
+	EventPattern="{\"source\":[\"aws.ec2\"],\"detail-type\":[\"EC2 Instance State-change Notification\"],\"detail\":{\"state\":[\"stopped\"],\"instance-id\":[\"instanceID\"]}}",
+	State='ENABLED',
+	Description='Build AMI when triggered'
+)
 
 ec2 = session.resource('ec2')
 response = ec2.create_tags( 
