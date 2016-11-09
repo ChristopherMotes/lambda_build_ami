73d7f83822a9a7065ea76d314d1cd29cd7ceec83
Christopher Motes <motescj@aim.com>
2016-11-09 17:35:41 -0600

    more troubleshooting updates

--- ami_image_create.py
+++ ami_image_create.py
@@ -1,21 +1,27 @@
-    import boto3
-    import time
-    instanceID='i-00e93862323dd6d88'
+import boto3
+import time
+def lambda_handler(event, context):
+    instanceID=event['detail']['instance-id']
     DATEYMD = time.strftime("%Y%m%d")
     ec2 = boto3.client('ec2')
     Name_string = "test-ami-%s" % DATEYMD
     response = ec2.create_image(
-    	InstanceId=instanceID,
-    	Name=Name_string,
-    	Description='Test of ami autobuild',
-    	NoReboot=False
+        InstanceId=instanceID,
+        Name=Name_string,
+        Description='Test of ami autobuild',
+        NoReboot=False
     )
     response = ec2.terminate_instances(
-    	InstanceIds=[ instanceID, ]
-    	
+        InstanceIds=[ instanceID, ]
+
     )
-    
+
     events = boto3.client('events')
-    response = events.delete_rule(
-    	Name='ami-auto-build',
+    response = events.remove_targets(
+        Rule='ami-auto-build',
+        Ids=[ '1', ]
     )
+    response = events.delete_rule(
+        Name='ami-auto-build',
+    )
+    return 'Function Complete'
