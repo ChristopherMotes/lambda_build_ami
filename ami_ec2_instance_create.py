cb6351b20cd52c8c61712ce952b79795935e0073
Christopher Motes <motescj@aim.com>
2016-11-08 18:49:47 -0600

    ec2 code running, failing on permissions

--- ami_ec2_instance_create.py
+++ ami_ec2_instance_create.py
@@ -1,47 +1,48 @@
 import boto3
+def lambda_handler(event, context):
+    TAG_Name='test-tag'
+    TAG_VPC='default'
+    imageID=event['id']
+    User_Data='IyEvYmluL2Jhc2gKIyMgQmFzZTY0IGVuY29kZSBtZSwgdGhlbiBhcHBseSBiYXNlNjQgb3V0cHV0IHRvIHRoZSB1c2VyIGRhdGEgc2VjdGlvbgojIyBpbnNlcnQgbm9kZSBidWlsZCBzY3JpcHQKaW5zdGFuY2VpZD1gY3VybCAtcyBjdXJsIGh0dHA6Ly8xNjkuMjU0LjE2OS4yNTQvbGF0ZXN0L21ldGEtZGF0YS9pbnN0YW5jZS1pZGAgCk5BTUU9YGF3cyAtLXJlZ2lvbiB1cy1lYXN0LTEgZWMyIGRlc2NyaWJlLXRhZ3MgLS1maWx0ZXJzICJOYW1lPXJlc291cmNlLWlkLFZhbHVlcz0kaW5zdGFuY2VpZCIgIk5hbWU9a2V5LFZhbHVlcz1OYW1lIiAtLXF1ZXJ5ICdUYWdzWzBdLlZhbHVlJyAtLW91dHB1dCB0ZXh0YCAKc3VkbyB5dW0gLXkgaW5zdGFsbCBodHRwczovL3l1bS5wdXBwZXRsYWJzLmNvbS9wdXBwZXRsYWJzLXJlbGVhc2UtcGMxLWVsLTYubm9hcmNoLnJwbQpzdWRvIHl1bSBjbGVhbiBhbGwKc3VkbyB5dW0gLXkgaW5zdGFsbCBwdXBwZXQtYWdlbnQKeXVtIC1xIC15IHVwZGF0ZQppbml0IDAK'
 
-session = boto3.Session(profile_name='shaeking1')
-TAG_Name='test-tag'
-TAG_VPC='default'
-User_Data='IyEvYmluL2Jhc2gKIyMgQmFzZTY0IGVuY29kZSBtZSwgdGhlbiBhcHBseSBiYXNlNjQgb3V0cHV0IHRvIHRoZSB1c2VyIGRhdGEgc2VjdGlvbgojIyBpbnNlcnQgbm9kZSBidWlsZCBzY3JpcHQKaW5zdGFuY2VpZD1gY3VybCAtcyBjdXJsIGh0dHA6Ly8xNjkuMjU0LjE2OS4yNTQvbGF0ZXN0L21ldGEtZGF0YS9pbnN0YW5jZS1pZGAgCk5BTUU9YGF3cyAtLXJlZ2lvbiB1cy1lYXN0LTEgZWMyIGRlc2NyaWJlLXRhZ3MgLS1maWx0ZXJzICJOYW1lPXJlc291cmNlLWlkLFZhbHVlcz0kaW5zdGFuY2VpZCIgIk5hbWU9a2V5LFZhbHVlcz1OYW1lIiAtLXF1ZXJ5ICdUYWdzWzBdLlZhbHVlJyAtLW91dHB1dCB0ZXh0YCAKc3VkbyB5dW0gLXkgaW5zdGFsbCBodHRwczovL3l1bS5wdXBwZXRsYWJzLmNvbS9wdXBwZXRsYWJzLXJlbGVhc2UtcGMxLWVsLTYubm9hcmNoLnJwbQpzdWRvIHl1bSBjbGVhbiBhbGwKc3VkbyB5dW0gLXkgaW5zdGFsbCBwdXBwZXQtYWdlbnQKeXVtIC1xIC15IHVwZGF0ZQppbml0IDAK'
 
+    ec2 = boto3.client('ec2')
+    instanceDict = ec2.run_instances(
+        ImageId=imageID,
+        KeyName='keypair-311',
+        MinCount=1,
+        MaxCount=1,
+        UserData=User_Data,
+            InstanceType='t2.micro',
+        NetworkInterfaces=[
+                {
+                        'DeviceIndex' : 0,
+                        'SubnetId' : 'subnet-5d384f60',
+                        'Groups' : [ 'sg-a886c2d2' ],
+                        'AssociatePublicIpAddress': True,
+                }
+        ],
+        IamInstanceProfile={ 'Arn': 'arn:aws:iam::742758411692:instance-profile/ami-auto-create', },
+    )
 
-ec2 = session.client('ec2')
-instanceDict = ec2.run_instances(
-	ImageId='ami-b73b63a0',
-	KeyName='keypair-311',
-	MinCount=1,
-	MaxCount=1,
-	UserData=User_Data,
-        InstanceType='t2.micro',
-	NetworkInterfaces=[ 
-		{
-			'DeviceIndex' : 0,
-			'SubnetId' : 'subnet-5d384f60',
-        		'Groups' : [ 'sg-a886c2d2' ],
-			'AssociatePublicIpAddress': True,
-		}
-	],
-	IamInstanceProfile={ 'Arn': 'arn:aws:iam::742758411692:instance-profile/ami-auto-create', },
-)
-	
-for instance in instanceDict['Instances']:
-	instanceID =  instance['InstanceId']
-Event_Pattern_string="{\"source\":[\"aws.ec2\"],\"detail-type\":[\"EC2 Instance State-change Notification\"],\"detail\":{\"state\":[\"stopped\"],\"instance-id\":[ \"%s\" ]}}" % instanceID
-events=session.client('events')
-response = events.put_rule(
-	Name='ami-auto-build',
-	EventPattern=Event_Pattern_string,
-	State='ENABLED',
-	Description='Build AMI when triggered'
-)
+    for instance in instanceDict['Instances']:
+        instanceID =  instance['InstanceId']
+    Event_Pattern_string="{\"source\":[\"aws.ec2\"],\"detail-type\":[\"EC2 Instance State-change Notification\"],\"detail\":{\"state\":[\"stopped\"],\"instance-id\":[ \"%s\" ]}}" % instanceID
+    events=boto3.client('events')
+    response = events.put_rule(
+        Name='ami-auto-build',
+        EventPattern=Event_Pattern_string,
+        State='ENABLED',
+        Description='Build AMI when triggered'
+    )
+   
+    ec2 = boto3.resource('ec2')
+    response = ec2.create_tags(
+        Resources=[ instanceID, ] ,
+        Tags=[
+                { 'Key' : 'Name', 'Value' : TAG_Name },
+                { 'Key' : 'AMI_Build', 'Value' : 'True' },
+        ]
+    )
 
-ec2 = session.resource('ec2')
-response = ec2.create_tags( 
-	Resources=[ instanceID, ] ,
-	Tags=[
-		{ 'Key' : 'Name', 'Value' : TAG_Name },
-		{ 'Key' : 'AMI_Build', 'Value' : 'True' },
-	]
-)
-
+    return 'Image Funtion Complete'
